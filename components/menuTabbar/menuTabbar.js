49c57254e51fbfbd9e73112e1dced383e9c97ab8
emmLi <601246372@qq.com>
2021-01-21 21:49:04 +0800

    底部导航菜单开发完成

--- components/menuTabbar/menuTabbar.js
+++ components/menuTabbar/menuTabbar.js
@@ -1,7 +1,7 @@
 // components/menuTabbar/menuTabbar.js
 Component({
-  options:{
-    styleIsolation:"apply-shared", // 组件样式对外隔离
+  options: {
+    styleIsolation: "apply-shared", // 组件样式对外隔离
   },
   /**
    * 组件的属性列表
@@ -14,17 +14,24 @@ Component({
    * 组件的初始数据
    */
   data: {
-    menuCurrent:1
+    menuCurrent: 1
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    menuClick(e){
-      console.log(e.currentTarget.dataset.params)
+    menuClick(e) {
+      let routeUrl = ["/pages/index/index", "/pages/mapDemo/mapDemo", "/pages/my/my"]
       this.setData({
-        menuCurrent:e.currentTarget.dataset.params
+        menuCurrent: e.currentTarget.dataset.params
+      })
+
+      wx.navigateTo({
+        url: routeUrl[e.currentTarget.dataset.params],
+        complete: function (com) {
+          // console.log(com)
+        }
       })
     }
   }
