cff30be4d317ee6ba9dcc7013761d623f9336da8
lee <601246372@qq.com>
2021-08-06 12:58:23 +0800

    up

--- pages/my/my.js
+++ pages/my/my.js
@@ -20,9 +20,13 @@ Page({
       "province": "",
       "country": "Iraq",
       "avatarUrl": null
-    }
+    },
+    accountVersion:"1.0.0", // 小程序版本号
   },
   onReady() {
+    // 获取小程序版本信息并检查更新
+    this.updateVersion()
+
     // 页面准备完成执行我的页面动画上推效果
     setTimeout(() => {
       this.setData({
@@ -49,6 +53,41 @@ Page({
         this.logout()
       }, 1000);
     }
+  },
+  // 检查更新
+  updateVersion(){
+    const accountInfo = wx.getAccountInfoSync();
+    console.log(accountInfo)
+    this.setData({
+      accountVersion:accountInfo.miniProgram.version || "1.0.0"
+    })
+    const updateManager = wx.getUpdateManager()
+
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+      console.log(res.hasUpdate)
+    })
+    
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: function (res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+    
+    updateManager.onUpdateFailed(function () {
+      // 新版本下载失败
+      wx.showToast({
+        title: '版本更新失败',
+        icon:"error"
+      })
+    })
   },
   // 退出登录
   logout: function (e) {
